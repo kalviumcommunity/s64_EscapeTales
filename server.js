79497f8b892cdf4a0dc7cb6d6f45fea2c220e6d3
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-02-05 12:45:07 +0530

    Mongo connected successfully

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./MongoDB");
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/Ping', (req, res) => {
-    res.send('Pong');
-    });
+// Connect to MongoDB
+connectDB();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    });
+app.get("/", (req, res) => {
+    res.send("MongoDB Atlas is connected to VS Code!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
