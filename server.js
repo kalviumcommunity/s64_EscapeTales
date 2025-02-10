3d5c58f5368fa928625603c573618ffef447d45c
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-02-10 23:34:23 +0530

    CRUD done

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const connectDB = require("./MongoDB");
+const cors = require("cors");
+const connectDB = require("./MongoDB"); // Import the MongoDB connection function
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
 
-// Connect to MongoDB
+// Connect to MongoDB Atlas
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("MongoDB Atlas is connected to VS Code!");
-});
+app.use("/api", require("./routes"));  // Import and use routes
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
